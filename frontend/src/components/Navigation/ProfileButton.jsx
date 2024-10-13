630a61f132b7f7edc43642cc1f77e1d96fc52349
JohnW44 <johnwintz@gmail.com>
2024-10-13 15:17:58 -0700

    completed authme

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -2,9 +2,11 @@
 
 import { useState, useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
-import { FaUserCircle, FaCarrot } from 'react-icons/fa';
+import { FaUserCircle } from 'react-icons/fa';
 import * as sessionActions from '../../store/session';
-
+import OpenModalButton from '../OpenModalButton/OpenModalButton';
+import LoginFormModal from '../LoginFormModal/LoginFormModal';
+import SignupFormPage from '../SignupFormModal/SignupFormModal';
 
 
 
@@ -31,32 +33,53 @@ function ProfileButton({ user }) {
     
         return () => document.removeEventListener('click', closeMenu);
       }, [showMenu]);
+
+      const closeMenu = () => setShowMenu(false)
     
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
+    closeMenu();
   };
 
   const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
 
   return (
     <>
-       <button onClick={toggleMenu}>
+      <button onClick={toggleMenu}>
         <FaUserCircle />
-        <FaCarrot style={{ color: "orange", marginLeft: "8px"}} />
       </button>
       <ul className={ulClassName} ref={ulRef}>
-        <li>{user.username}</li>
-        <li>{user.firstName} {user.lastName}</li>
-        <li>{user.email}</li>
-        <li>
-          <button onClick={logout}>Log Out</button>
-        </li>
-      </ul>
-    </>
-  );
+        {user ? (
+          <>
+            <li>{user.username}</li>
+            <li>{user.firstName} {user.lastName}</li>
+            <li>{user.email}</li>
+            <li>
+              <button onClick={logout}>Log Out</button>
+            </li>
+          </>
+        ) : (
+          <>
+          <li>
+            <OpenModalButton
+              buttonText="Log In"
+              onButtonClick={closeMenu}
+              modalComponent={<LoginFormModal />}
+            />
+          </li>
+          <li>
+            <OpenModalButton
+              buttonText="Sign Up"
+              onButtonClick={closeMenu}
+              modalComponent={<SignupFormModal />}
+            />
+          </li>
+        </>
+      )}
+    </ul>
+  </>
+);
 }
-
-
 
 export default ProfileButton;
--- frontend/src/components/OpenModalButton/OpenModalButton.jsx
+++ frontend/src/components/OpenModalButton/OpenModalButton.jsx
@@ -1,22 +1,25 @@
 // frontend/src/components/OpenModalButton/OpenModalButton.jsx
 
+import React from 'react';
 import { useModal } from '../../context/Modal';
 
-function OpenModalButton({
-  modalComponent, // component to render inside the modal
-  buttonText, // text of the button that opens the modal
-  onButtonClick, // optional: callback function that will be called once the button that opens the modal is clicked
-  onModalClose // optional: callback function that will be called once the modal is closed
-}) {
-  const { setModalContent, setOnModalClose } = useModal();
+function OpenModalMenuItem({
+    modalComponent, // component to render inside the modal
+    itemText, // text of the menu item that opens the modal
+    onItemClick, // optional: callback function that will be called once the menu item that opens the modal is clicked
+    onModalClose // optional: callback function that will be called once the modal is closed
+  }) {
+    const { setModalContent, setOnModalClose } = useModal();
 
-  const onClick = () => {
-    if (onModalClose) setOnModalClose(onModalClose);
-    setModalContent(modalComponent);
-    if (typeof onButtonClick === "function") onButtonClick();
-  };
+    const onClick = () => {
+        if (onModalClose) setOnModalClose(onModalClose);
+        setModalContent(modalComponent);
+        if (typeof onItemClick === "function") onItemClick();
+      };
 
-  return <button onClick={onClick}>{buttonText}</button>;
+  return (
+    <li onClick={onClick}>{itemText}</li>
+  );
 }
 
 export default OpenModalButton;
